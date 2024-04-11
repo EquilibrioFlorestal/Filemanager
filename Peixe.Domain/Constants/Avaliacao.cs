33ffc08aac01b86d73c0eb09caa488932a1bac7c
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2024-04-11 11:38:59 -0300

    [feat] Adicionado mapeamento padrão para avaliações.
    Adicionado mapeamento padrão para os blocos dos arquivos de SMQ.

---
+++ Peixe.Domain/Constants/Avaliacao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Domain.Constants
+{
+    public class Avaliacao
+    {
+        public static Dictionary<string, List<string>> NomePastaAvaliacao = new Dictionary<string, List<string>>
+        {
+            { "Colheita", ["Colheita"] },
+            { "Formiga Manual", ["Formiga Manual e Operacional"]},
+            { "Formiga Mecanizada", ["Formiga Mecanizada"] },
+            { "Irrigacao", ["Irrigacao"] },
+            { "Adubação de Base de Precisão e Pulverização", ["Adubação de Base de Precisão e Pulverização"] },
+            { "Pulverizador", ["Pulverizador"] },
+            { "Pulverizacao", ["Pulverizacao"] },
+            { "Preparo de Solo", ["Preparo de Solo"] },
+            { "Plantio Operacional", ["Plantio Operacional"] },
+            { "Baldeio", ["Baldeio"] },
+            { "Adubação cobertura", ["Adubação de cobertura Mecanizada"] },
+            { "90 dias", ["Sobrevivencia", "90 Dias"] },
+            { "30 dias", ["Sobrevivencia", "30 Dias"] }
+        };
+    }
+}
