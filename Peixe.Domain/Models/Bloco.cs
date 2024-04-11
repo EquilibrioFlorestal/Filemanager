452a56b166945f142e844457b6618d8203ff0560
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2024-04-11 11:32:06 -0300

    [feat] adicionado IdCiclo no modelo do Bloco.
    Adicionado campo referente ao IdCiclo no modelo do bloco.

---
+++ Peixe.Domain/Models/Bloco.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models
+{
+    public class Bloco
+    {
+        public int Id { get; set; }
+        public int IdCiclo { get; set; }
+        public required string Descricao { get; set; }
+    }
+}
